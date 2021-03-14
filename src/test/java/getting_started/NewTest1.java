cc7fe26efeff23d03c2bf0446b808e1fc9c26733
prachu271 <prachu271@gmail.com>
2021-03-14 18:58:41 +0530

    Json response code

--- src/test/java/getting_started/NewTest1.java
+++ src/test/java/getting_started/NewTest1.java
@@ -2,6 +2,7 @@ package getting_started;
 
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class NewTest1 {
 	
@@ -9,5 +10,18 @@ public class NewTest1 {
   public void firstGetRequest() {
 	  given().baseUri("https://restcountries.eu/rest/v2/").when().get("all").then().statusCode(200);
 	  
+  }
+  
+  @Test
+  public void firstJsonResponse() {
+	  given().baseUri("https://restcountries.eu/rest/v2").
+	  when().get("/alpha/USA").
+	  then().statusCode(200)
+	        .body(
+	        		"name",equalTo("United States of America"),
+	        		"alpha2Code",equalTo("US"),
+	        		"altSpellings",hasItem("USA"),
+	        		"currencies[0].symbol",equalTo("$")
+	              );
   }
 }
